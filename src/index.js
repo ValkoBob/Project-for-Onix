b0260e1866a0bff4405e43c2a6f42b6cf1aa802d
Valentyn Bobyr <vbobyr@vob.com>
2019-07-12 17:22:41 +0300

    Made fourth task.

--- src/index.js
+++ src/index.js
@@ -40,10 +40,10 @@ class HelloWorld extends React.Component {
             <footer>
               <div className="socials">
                 <div className="git">
-                  <a href={ this.state.gitHub }><i className="fab fa-github"/></a>
+                  <a href={this.state.gitHub}><i className="fab fa-github"/></a>
                 </div>
                 <div className="link_to_original">
-                  <a href={ this.state.linkToOriginal }><i className="far fa-image"/></a>
+                  <a href={this.state.linkToOriginal}><i className="far fa-image"/></a>
                 </div>
               </div>
               <h2 id="author">
