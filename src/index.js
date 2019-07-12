063a36bf1fbed5dc3031cfd70dcbd07001a3b699
Valentyn Bobyr <vbobyr@vob.com>
2019-07-12 16:50:54 +0300

    Made fourth task.

--- src/index.js
+++ src/index.js
@@ -2,45 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function HelloWorld() {
+const projectName = 'My hobbies:';
+
+class HelloWorld extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authorName: 'Bobyr Valentyn',
+      gitHub: 'https://github.com/ValkoBob',
+      linkToOriginal: 'https://dribbble.com/shots/6707868-Art-Exhibition'
+    };
+  }
+
+  render() {
     return (
-    <section className="container">
-        <div className="first_page">
+        <section className="container">
+          <div className="first_page">
             <header>
-                <a className="logo">Logo</a>
-                <nav className="menu">
-                    <a href="#">Project</a>
-                    <a href="#">About</a>
-                    <a href="#">Author</a>
-                </nav>
+              <a className="logo">Logo</a>
+              <nav className="menu">
+                <a href="#">Project</a>
+                <a href="#">About</a>
+                <a href="#">Author</a>
+              </nav>
             </header>
             <div className="info-text-block" id="project">
-                <h1>My hobbies:</h1>
-                <ul>
-                    <li>history;</li>
-                    <li>political philosophy;</li>
-                    <li>programming;</li>
-                    <li>european soccer;</li>
-                    <li>like to play computer games.</li>
-                </ul>
-                <button>Learn more</button>
+              <h1>{projectName}</h1>
+              <ul>
+                <li>history;</li>
+                <li>political philosophy;</li>
+                <li>programming;</li>
+                <li>european soccer;</li>
+                <li>like to play computer games.</li>
+              </ul>
+              <button>Learn more</button>
             </div>
             <footer>
-                <div className="socials">
-                    <div className="git">
-                        <a href="https://github.com/ValkoBob"><i className="fab fa-github"/></a>
-                    </div>
-                    <div className="link_to_original">
-                        <a href="https://dribbble.com/shots/6707868-Art-Exhibition"><i className="far fa-image"/></a>
-                    </div>
+              <div className="socials">
+                <div className="git">
+                  <a href={ this.state.gitHub }><i className="fab fa-github"/></a>
                 </div>
-                <h2 id="author">
-                    Author: Bobyr Valentyn
-                </h2>
+                <div className="link_to_original">
+                  <a href={ this.state.linkToOriginal }><i className="far fa-image"/></a>
+                </div>
+              </div>
+              <h2 id="author">
+                Author: {this.state.authorName}
+              </h2>
             </footer>
-        </div>
-    </section>
+          </div>
+        </section>
     )
+  }
 }
 
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+ReactDOM.render(<HelloWorld/>, document.getElementById('root'));
